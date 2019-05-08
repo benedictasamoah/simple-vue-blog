bc057375bcc57dee9ec4d424f029739576599bfc
Benedict Asamoah <benedict@sturta.com>
2019-05-08 11:15:16 +0000

    nav menu

---
+++ src/components/nav/index.js
@@ -0,0 +1,7 @@
+export default{
+    data () {
+        return {
+            nav: false
+        }
+    }
+}
--- src/components/nav/index.vue
+++ src/components/nav/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <Row class="nav" type="flex" justify="center">
-    <Col :xs="23" :sm="20" :md="20" :lg="20">
-      <Icon type="navicon"/>
-    </Col>
-  </Row>
+  <div>
+    <Row class="nav" type="flex" justify="center">
+      <Col :xs="23" :sm="20" :md="20" :lg="20">
+        <Icon type="navicon" @click.native="nav !== true ? nav = true : nav = false"/>
+      </Col>
+    </Row>
+    <div v-if="nav" class="navlist">
+      <Icon type="close" @click.native="nav !== true ? nav = true : nav = false"></Icon>
+      <ul>
+        <li>Menu 1</li>
+        <li>Menu 2</li>
+        <li>Menu 3</li>
+        <li>Menu 4</li>
+      </ul>
+    </div>
+  </div>
 </template>
+<script src="./index.js"/>
 <style scoped src="./index.css" />
